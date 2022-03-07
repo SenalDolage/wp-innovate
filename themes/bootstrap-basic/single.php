0369c5e72326eb2de5384ceb83282a15153fa910
Senal Dolage <senal.dolage@gmail.com>
2022-03-08 00:32:17 +0530

    single post page updated with redirect linked

--- themes/bootstrap-basic/single.php
+++ themes/bootstrap-basic/single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template for displaying single post (read full post page).
  * 
@@ -11,32 +12,43 @@ get_header();
  * determine main column size from actived sidebar
  */
 $main_column_size = bootstrapBasicGetMainColumnSize();
-?> 
-<?php get_sidebar('left'); ?> 
-				<div class="col-md-<?php echo $main_column_size; ?> content-area" id="main-column">
-					<main id="main" class="site-main" role="main">
-						<?php 
-						while (have_posts()) {
-							the_post();
+?>
+<?php get_sidebar('left'); ?>
+<div class="content-area" id="main-column">
+	<main id="main" class="site-main" role="main">
+		<div class="container">
+			<?php
+			while (have_posts()) {
+				the_post();
 
-							get_template_part('content', get_post_format());
+				get_template_part('content', get_post_format());
+			?>
+				<?php if (get_field('redirect_url')) { ?>
+					<div class="visit-product-here-wrp">
+						<h2>
+							Visit This Product
+							<a target="_blank" href="<?php the_field('redirect_url'); ?>">
+								Here.
+							</a>
+						</h2>
+					</div>
+				<?php } ?>
+			<?php
+				echo "\n\n";
 
-							echo "\n\n";
-							
-							bootstrapBasicPagination();
+				bootstrapBasicPagination();
 
-							echo "\n\n";
-							
-							// If comments are open or we have at least one comment, load up the comment template
-							if (comments_open() || '0' != get_comments_number()) {
-								comments_template();
-							}
+				echo "\n\n";
 
-							echo "\n\n";
+				// If comments are open or we have at least one comment, load up the comment template
+				if (comments_open() || '0' != get_comments_number()) {
+					comments_template();
+				}
 
-						} //endwhile;
-						?> 
-					</main>
-				</div>
-<?php get_sidebar('right'); ?> 
-<?php get_footer(); ?> 
+				echo "\n\n";
+			} //endwhile;
+			?>
+		</div>
+	</main>
+</div>
+<?php get_footer(); ?>
