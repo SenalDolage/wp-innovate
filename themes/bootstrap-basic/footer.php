fac83abd86e68c41621c905719a7781390613b6d
Senal Dolage <senal.dolage@gmail.com>
2022-03-10 17:45:23 +0530

    cf7 shortcode changed

--- themes/bootstrap-basic/footer.php
+++ themes/bootstrap-basic/footer.php
@@ -125,7 +125,7 @@
 						<h4>
 							NEWSLETTER
 						</h4>
-						<?php echo do_shortcode('[contact-form-7 id="189" title="Contact form 1"]'); ?>
+						<?php echo do_shortcode('[contact-form-7 id="189" title="Footer Newsletter"]'); ?>
 					</div>
 				</div>
 			</div>
