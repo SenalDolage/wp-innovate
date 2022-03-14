7861bcaf346213debeed8d02f1de604d0291253c
Senal Dolage <senal.dolage@gmail.com>
2022-03-14 12:52:21 +0530

    about template added

---
+++ themes/bootstrap-basic/template-about.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Template Name: About
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="range-page-with-slider-content">
+
+    <!-- Banner Section -->
+    <div class="range-banner">
+        <div class="container">
+            <?php if (get_field('about_banner_image')) :
+                $image = get_field('about_banner_image');
+            ?>
+                <div class="image-wrapper">
+                    <img src="<?php echo esc_url($image); ?>" alt="banner" />
+                </div>
+            <?php endif ?>
+        </div>
+    </div>
+
+    <div class="products-listing-wrapper">
+        <div class="products-inner-wrapper">
+            <div class="container">
+                <div class="row items-row">
+                    <div class="col-md-4">
+                        <div class="main-cat-name">
+
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="products-slider-wrapper">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+<?php get_footer(); ?>
